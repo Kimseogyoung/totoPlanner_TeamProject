d171d4368843d7080592b476a938de0a967228e1
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-07 19:47:38 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/teamproject_toto/MainActivity.java
+++ app/src/main/java/com/example/teamproject_toto/MainActivity.java
@@ -19,23 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
+        //프래그먼트2 타임라인으로 전환
         //getSupportFragmentManager().beginTransaction().replace(R.id.fragment, timelineFragment).commit();
 
         if(user==null){
-            //현재 로그인되어있지않다면
+            //현재 로그인되어있지않다면 로그인화면으로
             myStartActivity(loginActivity.class);
         }
         else{
             //로그인되어있으면 현재화면 유지
 
 
-            //아직 데이터베이스에 저장한 내용 받아오는거 없음
+            //아직 데이터베이스에 저장한 내용 받아오는거 없음 추가예정일듯
         }
 
-        findViewById(R.id.logout_btn).setOnClickListener(onClickListener);
+        //로그아웃버튼
+        //findViewById(R.id.logout_btn).setOnClickListener(onClickListener);
 
     }
-
+/* 클릭 이벤트리스너 (로그아웃 버튼 클릭 포함)
     View.OnClickListener onClickListener= new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+    */
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
