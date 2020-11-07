932a0cb5956321df4da1b94e788fd20ab3263f36
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-07 19:36:30 +0900

    srfdsfds

--- app/src/main/java/com/example/teamproject_toto/MemberInfo.java
+++ app/src/main/java/com/example/teamproject_toto/MemberInfo.java
@@ -2,9 +2,21 @@ package com.example.teamproject_toto;
 
 public class MemberInfo {
     private String name;
-    public MemberInfo(String n){
-        name=n;
+    private String phoneNumber;
+
+    public MemberInfo(String name, String phoneNumber){
+        this.name=name;
+        this.phoneNumber=phoneNumber;
     }
+
     public String getName(){return name;}
     public void setName(String n){name=n;}
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
