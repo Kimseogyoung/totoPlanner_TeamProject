fec8ac977d2721bf56f2d4bfa993a70b175f4cb1
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-07 19:30:11 +0900

    Revert "sad"
    This reverts commit faebc9ed1d446b2e9af0824fdc3f4660435768a9.

--- app/src/main/java/com/example/teamproject_toto/MemberInfo.java
+++ app/src/main/java/com/example/teamproject_toto/MemberInfo.java
@@ -2,21 +2,9 @@ package com.example.teamproject_toto;
 
 public class MemberInfo {
     private String name;
-    private String phoneNumber;
-
-    public MemberInfo(String name, String phoneNumber){
-        this.name=name;
-        this.phoneNumber=phoneNumber;
+    public MemberInfo(String n){
+        name=n;
     }
-
     public String getName(){return name;}
     public void setName(String n){name=n;}
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
--- app/src/main/java/com/example/teamproject_toto/signupActivity.java
+++ app/src/main/java/com/example/teamproject_toto/signupActivity.java
@@ -3,7 +3,6 @@ package com.example.teamproject_toto;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -11,15 +10,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class signupActivity extends Activity {
     private FirebaseAuth mAuth;
@@ -56,12 +51,11 @@ public class signupActivity extends Activity {
 
     private void signup(){
         final String name = ((EditText)findViewById(R.id.signup_name)).getText().toString();
-        final String phoneNumber = ((EditText)findViewById(R.id.signup_number)).getText().toString();
         String email = ((EditText)findViewById(R.id.signup_email)).getText().toString();
         String password = ((EditText)findViewById(R.id.signup_password)).getText().toString();
         String passwordCheck = ((EditText)findViewById(R.id.signup_password2)).getText().toString();
 
-        if(name.length()>0 &&phoneNumber.length()>0 && email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
+        if(name.length()>0 && email.length() > 0 && password.length() > 0 && passwordCheck.length() > 0){
             if(password.equals(passwordCheck)){
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -69,7 +63,9 @@ public class signupActivity extends Activity {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
-                                    profileUpdate(name,phoneNumber);
+                                    Toast.makeText(getApplicationContext(),"회원가입이 성공했습니다.",Toast.LENGTH_SHORT).show();
+                                    profileUpdate(name);
+
                                     //UI
                                 } else {
                                     // If sign in fails, display a message to the user.
@@ -93,31 +89,25 @@ public class signupActivity extends Activity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
-    private void profileUpdate(String name, String phoneNumber){
-
-        //데이터베이스에 회원정보 등록
+    private void profileUpdate(String name){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        MemberInfo memberInfo =new MemberInfo(name,phoneNumber);
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(name)
+                .build();
+
         if(user!=null){
-            db.collection("users").document(user.getUid()).set(memberInfo)
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+            user.updateProfile(profileUpdates)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
-                        public void onSuccess(Void aVoid) {
-                            Toast.makeText(getApplicationContext(),"회원가입에 성공했습니다.",Toast.LENGTH_SHORT).show();
-                        }
-
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Error adding document", e);
-                            Toast.makeText(getApplicationContext(),"회원가입에 실패했습니다."+e.toString(),Toast.LENGTH_SHORT).show();
-
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                               //업데이트 완료
+                            }
                         }
                     });
         }
+
     }
 }
 
