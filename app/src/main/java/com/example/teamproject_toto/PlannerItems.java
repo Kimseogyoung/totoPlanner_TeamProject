043ff6680688a382ad7205a3d1f3d05a53f3d4a8
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2020-11-12 20:49:51 +0900

    안올려진것들 추가

---
+++ app/src/main/java/com/example/teamproject_toto/PlannerItems.java
@@ -0,0 +1,16 @@
+package com.example.teamproject_toto;
+
+
+public class PlannerItems  {
+
+    private String text ;
+
+    public void setText(String text) {
+        this.text = text ;
+    }
+
+
+    public String getText() {
+        return this.text ;
+    }
+}
