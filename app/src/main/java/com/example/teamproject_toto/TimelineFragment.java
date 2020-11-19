2e43d7dbe396e0260d70f2f955d7f46ee5a34a01
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-19 15:27:14 +0900

    11/19 주석추가 seogyoung

--- app/src/main/java/com/example/teamproject_toto/TimelineFragment.java
+++ app/src/main/java/com/example/teamproject_toto/TimelineFragment.java
@@ -40,19 +40,20 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+//타임라인 프래그먼트
 public class TimelineFragment extends Fragment implements onBackPressedListener{
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.Adapter adapter;
     TimelineFragment f;
 
-    private ArrayList<TimelineboardInfo> boardList = new ArrayList<>();
+    private ArrayList<TimelineboardInfo> boardList = new ArrayList<>();//타임라인의 게시글 리스트
 
-    private static final String TAG="TimelineFragment";
+    private static final String TAG="TimelineFragment";//디버깅 태그
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    String username;
+    String username;//사용자 이름
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container
             , @Nullable Bundle savedInstanceState){
@@ -65,17 +66,17 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
 
 
         recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), 1));
-
+        //사용자이름 저장
         loadUsername();
-        //타임라인 데이터 업데이트
+        
+        //타임라인 새로고침
         update();
         return view;
 
     }
+    //타임라인 데이터를 읽고 어뎁터에 업데이트하는 함수(새로고침)
     public void update(){
-
-
-        db.collection("user-timeline").document(user.getUid()).collection("timeline")
+        db.collection("user-timeline").document(user.getUid()).collection("timeline")//타임라인컬렉션에 있는 모든 문서
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -83,7 +84,7 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
 
-                                Log.d(TAG, document.getId() + " => " + document.getData().get("name"));
+                                //boardList에 타임라인 게시글 정보 추가
                                 boardList.add(0,
                                         new TimelineboardInfo(""+document.getData().get("writercode"),
                                         ""+document.getData().get("name"),
@@ -93,14 +94,16 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
                                         ""+document.getData().get("content"),
                                         ""+document.getData().get("id")));
 
-
                             }
+                            //만약 게시글이 하나도없다면 텍스트뷰 출력
                             if(boardList.size()<1){
                                 getView().findViewById(R.id.noTimeline).setVisibility(View.VISIBLE);
                             }
-                            else{
+                            else{//하나라도 있다면 invisible
                                 getView().findViewById(R.id.noTimeline).setVisibility(View.INVISIBLE);
                             }
+                            
+                            //어댑터 업데이트
                             adapter = new TimelineAdapter(boardList,getContext(),f);
                             recyclerView.setAdapter(adapter);
                         } else {
@@ -111,40 +114,39 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
 
 
     }
-    //댓글 창을 열고 댓글작성할 수 있다.
+    //댓글 창을 열고 댓글작성하는 함수
     public void openCommentTap(final int index){
 
-        reloadComment(index);
-
+        reloadComment(index);//데이터베이스에 저장된 각 게시글의 댓글들을 읽어오고 출력
 
         final LinearLayout commenttap= getView().findViewById(R.id.timeline_commentTap);
         TextView titletext= getView().findViewById(R.id.timeline_commentTitle);
         Button btn=getView().findViewById(R.id.timeline_commentupload);
         final EditText editText=getView().findViewById(R.id.timeline_commentedit);
 
-        commenttap.setVisibility(View.VISIBLE);
+        commenttap.setVisibility(View.VISIBLE);//레이아웃 visible
 
 
-        //idx는 현재 누른 아이템
-        //boardlist에서 받아온 idx값으로 현재 보드의 id와 writercode를 알아내서
-        //user-writercode-timeline-id-comment경로로 코멘트작성
-
-
-        titletext.setOnClickListener(new View.OnClickListener() {
+       
+        titletext.setOnClickListener(new View.OnClickListener() {//"댓글"TextView가 클릭되면 댓글창 접기
             @Override
             public void onClick(View view) {
                 commenttap.setVisibility(View.INVISIBLE);
                 editText.setText("");
             }
         });
-        btn.setOnClickListener(new View.OnClickListener() {
+        
+         //idx는 현재 누른 아이템 인덱스
+        //boardlist에서 받아온 idx값으로 현재 보드의 id와 writercode를 알아내서
+        //user-writercode-timeline-id-comment경로로 코멘트를 작성할 수있다.
+        btn.setOnClickListener(new View.OnClickListener() {//댓글 작성버튼 클릭 시 이벤트 리스너
             @Override
             public void onClick(View view) {
-                SimpleDateFormat format = new SimpleDateFormat("MM월 dd일 HH:mm:ss");
+                SimpleDateFormat format = new SimpleDateFormat("MM월 dd일 HH:mm:ss");//작성시간 받아오기
                 Date now = new Date();
                 final String ss = format.format(now);
 
-                Map map = new HashMap<String, String>();
+                Map map = new HashMap<String, String>();//데이터 필드로 등록할 값 map으로 연결
                 map.put("text", editText.getText().toString());
                 map.put("time",ss);
                 map.put("name",username);
@@ -153,19 +155,20 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
                 String boardWritercode=boardList.get(index).getWritercode();
 
                 db.collection("users").document(""+boardWritercode)
-                        .collection("timeline").document(""+boardId).collection("comment")
+                        .collection("timeline").document(""+boardId).collection("comment")//해당 게시글 comment컬렉션에 문서 등록
                         .document(""+ss).set(map);
                 editText.setText("");
-                reloadComment(index);
+                reloadComment(index);//댓글 리로드
 
             }
         });
 
     }
     //댓글을 다시 로드하는 함수
-    public void reloadComment(int idx){
+    public void reloadComment(int idx){//idx 는 현재 선택한 게시글 번호
         final TextView commenttext=getView().findViewById(R.id.timeline_comment);
 
+        //해당게시글의 comment컬렉션을 접근하여 모든 댓글 document를 읽는다.
         db.collection("users").document(boardList.get(idx).getWritercode())
                 .collection("timeline").document(boardList.get(idx).getId()).collection("comment")
                 .get()
@@ -176,6 +179,7 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
                             String newcomment="";
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 if(document!=null){
+                                    //TextView에 댓글 출력
                                     newcomment += document.getData().get("time")+"  "+document.getData().get("name")+
                                         "\n"+document.getData().get("text")+"\n\n";
                                 }
@@ -186,6 +190,8 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
                 });
 
     }
+    
+    //사용자 이름을 받아오는 함수.
     private void loadUsername(){
         DocumentReference docRef = db.collection("users").document(user.getUid());
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -206,6 +212,7 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
     }
 
 
+    //첫번째 프래그먼트로 이동하는 함수.
     public void GoBack(){
         PlannerFragment fragment1=new PlannerFragment();
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
@@ -213,6 +220,7 @@ public class TimelineFragment extends Fragment implements onBackPressedListener{
 
     }
 
+    //뒤로가기 이벤트리스너
     @Override
     public void onBackPressed() {
         GoBack();
