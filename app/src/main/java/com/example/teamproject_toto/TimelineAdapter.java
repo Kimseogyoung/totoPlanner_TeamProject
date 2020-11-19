555f5eb3ce14e5972be20b82c0e3ead0ae9b607c
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-19 14:41:02 +0900

    11/19 주석추가 seogyoung

--- app/src/main/java/com/example/teamproject_toto/TimelineAdapter.java
+++ app/src/main/java/com/example/teamproject_toto/TimelineAdapter.java
@@ -23,16 +23,18 @@ import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
+//타임라인 리사이클러뷰의 어댑터
 public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ItemViewHolder> {
     private ArrayList<TimelineboardInfo> listData = new ArrayList<>();
     FirebaseStorage storage;
-    private Context context;
-    TimelineFragment fragment;
+    
+    private Context context;//프래그먼트의 context
+    TimelineFragment fragment;//타임라인 프래그먼트
 
     public TimelineAdapter(ArrayList<TimelineboardInfo> data, Context c,TimelineFragment f){
         context=c;
         listData=data;
-        storage= FirebaseStorage.getInstance("gs://todaytogether-8a723.appspot.com/");
+        storage= FirebaseStorage.getInstance("gs://todaytogether-8a723.appspot.com/");//파이어베이스 storage 링크
         fragment=f;
     }
 
@@ -40,7 +42,6 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ItemVi
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View holderView = LayoutInflater.from(parent.getContext()).inflate(R.layout.timelineitems, parent, false);
-
         return new ItemViewHolder(holderView);
     }
 
@@ -63,47 +64,48 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ItemVi
             }
         });
 
+        
 
-            //FirebaseStorage 인스턴스를 생성
-            FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
-            // 위의 저장소를 참조하는 파일명으로 지정
-            StorageReference storageReference = firebaseStorage.getReference().child("images/"+data.getImg());
-            //StorageReference에서 파일 다운로드 URL 가져옴
-            storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                    if (task.isSuccessful()) {
-                        holder.Img.setVisibility(View.VISIBLE);
-                        // Glide 이용하여 이미지뷰에 로딩
-                        Glide.with(context)
-                                .load(task.getResult())
-                                .into(holder.Img);
-                    } else {
-                        // URL을 가져오지 못하면
-                        holder.Img.setVisibility(View.GONE);
-                    }
-                }
-            });
-            StorageReference storageReference2 = firebaseStorage.getReference().child("profile/"+data.getWritercode());
-            //StorageReference에서 파일 다운로드 URL 가져옴
-            storageReference2.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
-                @Override
-                public void onComplete(@NonNull Task<Uri> task) {
-                     if (task.isSuccessful()) {
-                        // Glide 이용하여 이미지뷰에 로딩
-                        Glide.with(context)
+        //게시글 첨부 이미지로드
+        //FirebaseStorage 인스턴스를 생성
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        // 위의 저장소를 참조하는 파일명으로 지정
+        StorageReference storageReference = firebaseStorage.getReference().child("images/"+data.getImg());
+        //StorageReference에서 파일 다운로드 URL 가져옴
+        storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    holder.Img.setVisibility(View.VISIBLE);
+                    // Glide 이용하여 이미지뷰에 로딩
+                    Glide.with(context)
                             .load(task.getResult())
-                            .into(holder.iconImg);
-                    }
-                     else{
-                         //못가져오면
-                         holder.iconImg.setImageResource(R.drawable.basic);
-                     }
+                            .into(holder.Img);
+                } else {
+                    // URL을 가져오지 못하면 위젯을 GONE으로 처리
+                    holder.Img.setVisibility(View.GONE);
                 }
-            });
-
-
-
+            }
+        });
+        
+        //게시글 작성자 프로필사진 로드
+        StorageReference storageReference2 = firebaseStorage.getReference().child("profile/"+data.getWritercode());
+        //StorageReference에서 파일 다운로드 URL 가져옴
+        storageReference2.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                 if (task.isSuccessful()) {
+                    // Glide 이용하여 이미지뷰에 로딩
+                    Glide.with(context)
+                        .load(task.getResult())
+                        .into(holder.iconImg);
+                    }
+                 else{
+                     //못가져오면
+                     holder.iconImg.setImageResource(R.drawable.basic);
+                 }
+            }
+        });
     }
 
     @Override
@@ -112,6 +114,7 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ItemVi
     }
 
 
+    //뷰홀더 클래스
     class ItemViewHolder extends RecyclerView.ViewHolder {
         public TextView nameText, dateText, titleText,contentText;
         public ImageView iconImg, Img;
@@ -119,7 +122,6 @@ public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.ItemVi
 
         ItemViewHolder(View itemView) {
             super(itemView);
-
             nameText= itemView.findViewById(R.id.item_name);
             dateText =itemView.findViewById(R.id.item_date);
             titleText=itemView.findViewById(R.id.item_title);
