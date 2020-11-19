ccefc92e5f197c619dfa7117443b0fcff01e6cf7
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-19 15:45:07 +0900

    11/19 주석 작성 김서경

--- app/src/main/java/com/example/teamproject_toto/onBackPressedListener.java
+++ app/src/main/java/com/example/teamproject_toto/onBackPressedListener.java
@@ -1,5 +1,6 @@
 package com.example.teamproject_toto;
 
+//onBackPressedListener.java 파일 코드 작성자: 김서경
 //프래그먼트에서 뒤로가기버튼을 오버라이딩 할 수 있게 하는 자바인터페이스
 public interface onBackPressedListener {
     void onBackPressed();
