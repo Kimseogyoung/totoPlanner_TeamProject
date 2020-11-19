aa32794d0d34bd68aba81a22354582503acc3e1b
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2020-11-19 15:31:39 +0900

    11/19 주석 추가

--- app/src/main/java/com/example/teamproject_toto/MainActivity.java
+++ app/src/main/java/com/example/teamproject_toto/MainActivity.java
@@ -27,9 +27,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
 
-
-
-        //사진찍기 권한
+        //사진찍기 권한 허용 설정
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 Log.d(TAG, "권한 설정 완료");
@@ -40,11 +38,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if(user==null){
-            //현재 로그인되어있지않다면 로그인화면으로
+            //현재 로그인되어있지않다면 로그인화면으로 이동
             myStartActivity(loginActivity.class);
         }
         else{
-            //로그인되어있으면 현재화면 유지
+            //로그인되어있으면 현재화면 유지하고, 일정 프래그먼트로 이동
             PlannerFragment fragment1 = new PlannerFragment();
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.mainFrame, fragment1);
@@ -67,26 +65,26 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             switch(view.getId()){
-                case R.id.planner_btn:
+                case R.id.planner_btn://일정버튼 클릭시
                     PlannerFragment fragment1 = new PlannerFragment();
                     transaction.replace(R.id.mainFrame, fragment1);
                     transaction.commit();
                     break;
 
-                case R.id.timeline_btn:
+                case R.id.timeline_btn://타임라인 버튼 클릭시
                     TimelineFragment fragment2 = new TimelineFragment();
                     transaction.replace(R.id.mainFrame, fragment2);
                     transaction.commit();
                     break;
 
-                case R.id.community_btn:
+                case R.id.community_btn://커뮤니티버튼 클릭시
                      CommunityFragment fragment = new CommunityFragment();
                     transaction.replace(R.id.mainFrame, fragment);
                     transaction.commit();
 
                     break;
 
-                case R.id.profileFriend_btn:
+                case R.id.profileFriend_btn:// 내 정보 버튼 클릭시
                     ProfileandFriendFragment fragment4 = new ProfileandFriendFragment();
 
                     transaction.replace(R.id.mainFrame, fragment4);
@@ -96,11 +94,13 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    //class c에 해당하는 액티비티로 이동하는 함수
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+   
     public void onBackPressed() {
 
         //프래그먼트 onBackPressedListener사용
