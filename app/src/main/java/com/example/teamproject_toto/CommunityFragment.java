530efe41ee9c5bbc8e7c779516ee42d9b7eadd0e
leeZn30 <55116981+leeZn30@users.noreply.github.com>
2020-11-20 12:28:59 +0900

    11/20 이아연 주석 작성

--- app/src/main/java/com/example/teamproject_toto/CommunityFragment.java
+++ app/src/main/java/com/example/teamproject_toto/CommunityFragment.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+// CommunityFragment.java 작성자 : 이아연
+// 여러 커뮤니티 중에서 어느 커뮤니티로 갈 것인지 결정함.
 public class CommunityFragment extends Fragment implements onBackPressedListener {
 
 
@@ -19,10 +21,11 @@ public class CommunityFragment extends Fragment implements onBackPressedListener
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_community, container, false);
 
-        TextView daily_life_tv = view.findViewById(R.id.daily_life_tv);
-        TextView employment_tv = view.findViewById(R.id.employment_tv);
-        TextView exercise_tv = view.findViewById(R.id.exercise_tv);
-        TextView smallhappy_tv = view.findViewById(R.id.smallhappy_tv);
+        TextView daily_life_tv = view.findViewById(R.id.daily_life_tv); // 일상 공유 커뮤니티
+        TextView employment_tv = view.findViewById(R.id.employment_tv); // 취업 커뮤니티
+        TextView exercise_tv = view.findViewById(R.id.exercise_tv); // 운동 커뮤니티
+        TextView smallhappy_tv = view.findViewById(R.id.smallhappy_tv); // 소확행 커뮤니티
+        // 소확행은 모든 사용자가 같은 날, 같은 소확행을 얻으므로 커뮤니티를 통해서 공유가능.
 
         daily_life_tv.setOnClickListener(OnClickListener);
         employment_tv.setOnClickListener(OnClickListener);
@@ -32,6 +35,7 @@ public class CommunityFragment extends Fragment implements onBackPressedListener
         return view;
     }
 
+    // 각 커뮤니티 TextView onClickListener
     View.OnClickListener OnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -39,29 +43,29 @@ public class CommunityFragment extends Fragment implements onBackPressedListener
             CommunityBoardFragment fragment = new CommunityBoardFragment();
             Bundle bundle = new Bundle();
             switch (view.getId()){
-                case R.id.daily_life_tv:
+                case R.id.daily_life_tv: // 일상 공유 커뮤니티
                     transaction.replace(R.id.mainFrame, fragment);
-                    bundle.putString("kinds", "daily-life");
+                    bundle.putString("kinds", "daily-life"); // 일상 공유 커뮤니티라는 것을 넘긴다.
                     fragment.setArguments(bundle);
                     transaction.commit();
                     break;
 
-                case R.id.employment_tv:
+                case R.id.employment_tv: // 취업 커뮤니티
                     transaction.replace(R.id.mainFrame, fragment);
-                    bundle.putString("kinds", "employment");
+                    bundle.putString("kinds", "employment"); // 취업 커뮤니티라는 것을 넘긴다.
                     fragment.setArguments(bundle);
                     transaction.commit();
                     break;
 
-                case R.id.exercise_tv:
-                    transaction.replace(R.id.mainFrame, fragment);
+                case R.id.exercise_tv: // 운동 커뮤니티
+                    transaction.replace(R.id.mainFrame, fragment); // 운동 커뮤니티라는 것을 넘긴다.
                     bundle.putString("kinds", "exercise");
                     fragment.setArguments(bundle);
                     transaction.commit();
                     break;
 
-                case R.id.smallhappy_tv:
-                    transaction.replace(R.id.mainFrame, fragment);
+                case R.id.smallhappy_tv: // 소확행 커뮤니티
+                    transaction.replace(R.id.mainFrame, fragment); // 소확행 커뮤니티라는 것을 넘긴다.
                     bundle.putString("kinds", "smallhappy");
                     fragment.setArguments(bundle);
                     transaction.commit();
@@ -69,7 +73,8 @@ public class CommunityFragment extends Fragment implements onBackPressedListener
             }
         }
     };
-
+    
+    // 뒤로 가기 메소드
     public void GoBack(){
         PlannerFragment fragment1=new PlannerFragment();
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
@@ -77,6 +82,7 @@ public class CommunityFragment extends Fragment implements onBackPressedListener
 
     }
 
+    // 핸드폰 뒤로 가기 클릭시 뒤로 가기 메소드 실행
     @Override
     public void onBackPressed() {
         GoBack();
